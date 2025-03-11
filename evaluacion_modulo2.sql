0d099f6a5d007fd4d38b5de8fd8c2d684530467c
Frailin Baez <marina93pg@gmail.com>
2025-03-11 18:14:53 +0100

    avances

--- evaluacion_modulo2.sql
+++ evaluacion_modulo2.sql
@@ -56,3 +56,45 @@ GROUP BY r.customer_id;
 
 /*11.Encuentra la cantidad total de películas alquiladas por categoría y muestra el nombre de la categoría
 junto con el recuento de alquileres.*/
+SELECT c.name, COUNT(r.inventory_id) AS 'Total alquileres'
+FROM category AS c
+LEFT JOIN film_category AS f
+USING (category_id)
+INNER JOIN inventory AS i
+USING (film_id)
+INNER JOIN rental AS r
+USING (inventory_id)
+GROUP BY c.name;
+
+/*12.Encuentra el promedio de duración de las películas para cada clasificación de la tabla film y
+muestra la clasificación junto con el promedio de duración.*/
+SELECT rating, ROUND(AVG(length), 0) AS 'Promedio duración'
+FROM film
+GROUP BY rating;
+
+-- 13.Encuentra el nombre y apellido de los actores que aparecen en la película con title "Indian Love".
+SELECT first_name, last_name
+FROM actor
+INNER JOIN film_actor
+USING (actor_id)
+INNER JOIN film
+USING (film_id) 
+WHERE film_id = (SELECT film_id
+					FROM film
+					WHERE title = "Indian Love");
+
+-- 14.Muestra el título de todas las películas que contengan la palabra "dog" o "cat" en su descripción.
+SELECT title
+FROM film
+WHERE description LIKE '%dog%' OR description LIKE '%cat%';
+
+-- 15.Hay algún actor o actriz que no aparezca en ninguna película en la tabla film_actor
+SELECT first_name, last_name
+FROM actor
+WHERE actor_id NOT IN (SELECT actor_id
+							FROM film_actor);
+-- No hay ningún actor que no aparezca en ninguna película
+
+-- 16.Encuentra el título de todas las películas que fueron lanzadas entre el año 2005 y 2010.
+SELECT title
+FROM film
